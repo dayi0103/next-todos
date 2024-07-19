1f8e1d66b1ab8336fea8db44f6d2b252ae75fe4b
tongle-docker <tongle-docker@tongle-dockerui-MacBookAir.local>
2024-07-19 19:49:35 +0900

    first commit

---
+++ components/loginButton.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+interface Props {
+  title?: string;
+  color?: string;
+  isBorder?: boolean;
+}
+
+const LoginButton: React.FC<Props> = ({
+  title = "",
+  color = "skyblue",
+  isBorder = false,
+}) => {
+  return (
+    <div
+      className={`${isBorder && "border"} w-[80px] text-center py-2 rounded-lg`}
+      style={{ color: color }}
+    >
+      {title}
+    </div>
+  );
+};
+
+export default LoginButton;
